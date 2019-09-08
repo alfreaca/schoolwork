66b8d1f6c6a72561ea6d63e7ca0094ebea3a1154
root <root@localhost.localdomain>
2019-09-08 16:07:00 +0100

    just small tweaks

--- introtask.py
+++ introtask.py
@@ -22,21 +22,24 @@ class Machine:
                 not_int = False
             except ValueError:
                 money_in = input("Please enter a number")
+
         return money_in
 
     def take_money(self):
         money_in = self.error_handler_input()
         while_done = False
         while (money_in % 10) != 0:
-            print("Your balance must be a multiple of 10\nPlease enter another coin:")
+            print("Your balance must be a multiple of 10\nPlease enter another coin")
             money_in = money_in + self.error_handler_input()
             print("Current balance is: " + str(money_in))
+            print("Current number of goes left: " + str(money_in//10))
             while_done = True
 
         self.balance = money_in
 
         if not while_done:
             print("Current Balance: " + str(self.balance))
+            print("Current number of goes left: " + self.balance//10)
             return
         else:
             return
@@ -44,18 +47,28 @@ class Machine:
     def game(self):
        pass
 
-    def exit(self):
+    def exit_method(self):
         print("Thanks for playing!")
         sleep(10)
         exit()
 
     def menu(self):
-        choice = ("What would you like to do? [a] insert more money, [b] play the game, or [c] exit")
+        choice = 'nothing'
+        choice = input("What would you like to do? [a] insert more money, [b] play the game, or [c] exit: ")
+        while choice != "a" and choice != "b" and choice != "c":
+            choice = input("Please choose either [a] insert more money, [b] play the game, or [c] exit: ")
+
+        if choice == 'a':
+            self.take_money()
+
+        if choice == 'b':
+            self.game()
         
-
-
-
-
+        if choice == 'c':
+            self.exit_method()
+            
 run = Machine(0, 0)
 
-run.take_money()
+#run.take_money()
+
+run.menu()
