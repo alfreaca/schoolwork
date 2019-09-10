bda3347e62c1e599c28705545e2a61858177aa55
alfreaca <atomlizard@gmail.com>
2019-09-10 20:38:30 +0100

    Added the actual game bit
    It doesn't actually work rn but I will fix it, promise

--- introtask.py
+++ introtask.py
@@ -3,16 +3,27 @@ from time import sleep
 """
 Slot machine
 Author: Kit
+
+7
+BAR
+Bell
+Cherry
+Watermelon
+Grapes
+horseshoe
+
 """
-
-
+# Defining Class
 class Machine:
 
+    # Constructor for the variables I think I will need - may change to be honest
     def __init__(self, balance, goes):
 
         self.balance = balance
         self.goes = goes
+        self.ready = 'null'
 
+    # This will make sure it doesn't error when something other than an int is put in.
     def error_handler_input(self):
         money_in = input("Enter the money: ")
         not_int = True
@@ -25,35 +36,105 @@ class Machine:
 
         return money_in
 
+    # Function to be called by menu method that will take the money
     def take_money(self):
-        money_in = self.error_handler_input()
+        self.balance += self.error_handler_input()
         while_done = False
-        while (money_in % 10) != 0:
+        while (self.balance % 10) != 0:
             print("Your balance must be a multiple of 10\nPlease enter another coin")
-            money_in = money_in + self.error_handler_input()
-            print("Current balance is: " + str(money_in))
-            print("Current number of goes left: " + str(money_in//10))
+            self.balance += self.error_handler_input()
+            print("Current balance is: " + str(self.balance))
+            print("Current number of goes left: " + str(self.balance//10))
             while_done = True
-
-        self.balance = money_in
 
         if not while_done:
             print("Current Balance: " + str(self.balance))
-            print("Current number of goes left: " + self.balance//10)
+            print("Current number of goes left: " + str(self.balance//10))
+            self.goes += self.balance//10
             return
         else:
+            self.goes += self.balance//10
             return
 
     def game(self):
-       pass
+        # Print the winning hands then pick 3 random numbers
+            print("The winning hands are:\n3 7s for 20, 3 BARs for 5, 3 Bells for 3, 3 Cherry's for 1.\n")
 
+            choices = ['7', 'BAR', 'Bell', 'Cherry', 'Watermelon', 'Grapes', 'Horseshoe']
+
+            self.ready = input("Enter Y to spin: ")
+
+            while self.goes > 0 and self.ready == 'y':
+                sleep(1)
+                first_choice = random.choice(choices)
+                print(first_choice)
+                sleep(1)
+                second_choice = random.choice(choices)
+                print(second_choice)
+                sleep(1)
+                third_choice = random.choice(choices)
+                print(third_choice+"\n")
+                sleep(1)
+
+                self.goes -= 1
+
+                if first_choice == '7' or first_choice == 'BAR' or first_choice == 'Bell' or first_choice == 'Cherry':
+
+                    if first_choice == second_choice == third_choice:
+
+                        if first_choice == '7':
+                            print("Congratulations, 3 7s. You've won £20 ")
+                            self.balance += 20
+                             break
+
+                        elif first_choice == 'BAR':
+                            print("Congratulations, 3 BARs. You've won £5")
+                            self.balance += 5
+                            break
+
+                        elif first_choice == 'Bell':
+                            print("Congratulations, 3 Bells. You've won £3")
+                            self.balance += 3
+                            break
+
+                        elif first_choice == 'Cherry':
+                            print("Congratulations, 3 Cherrys. You've won £1")
+                            self.balance += 1
+                            break
+
+                    else:
+                        print("Unlucky!")
+                        if self.goes == 1:
+                            print("You have " + str(self.goes) + " go left.\n\n")
+                        else:
+                            print("You have " + str(self.goes) + " goes left.\n\n")
+
+                        if self.goes == 0:
+                            break
+                        else:
+                            self.ready = input("Press Y to spin again: ")
+
+                else:
+                    print("Unlucky!")
+                    if self.goes == 1:
+                        print("You have " + str(self.goes) + " go left.\n\n")
+                    else:
+                        print("You have " + str(self.goes) + " goes left.\n\n")
+
+                    if self.goes == 0:
+                        break
+                    else:
+                        self.ready = input("Press Y to spin again: ")
+
+            break
+            
     def exit_method(self):
         print("Thanks for playing!")
-        sleep(10)
+        sleep(5)
         exit()
 
+    # Menu method to then select which method I need 
     def menu(self):
-        choice = 'nothing'
         choice = input("What would you like to do? [a] insert more money, [b] play the game, or [c] exit: ")
         while choice != "a" and choice != "b" and choice != "c":
             choice = input("Please choose either [a] insert more money, [b] play the game, or [c] exit: ")
@@ -66,9 +147,11 @@ class Machine:
         
         if choice == 'c':
             self.exit_method()
-            
+
+    def main(self):
+        while True:
+            self.menu()
+
 run = Machine(0, 0)
 
-#run.take_money()
-
-run.menu()
+run.main()
